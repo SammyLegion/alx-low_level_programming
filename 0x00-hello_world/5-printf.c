3e09f88fa51ece9b5bd656c3e6e2c14de7501528
root <sammyokorie0@gmail.com>
2022-04-07 14:44:45 -0700

    please

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, Print Programming is like building a multilingual puzzle
+ *
+ * Return: returns 0 if successful
+ *
+ */
+
+int main(void)
+{ 
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
