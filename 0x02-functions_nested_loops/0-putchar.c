71d97a64059988f89e16d37cd4a6b45567e9780b
Samuel Okorie <sammyokorie0@gmail.com>
2022-04-12 22:42:59 +0100

    nah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* main - Prints Holberton
-* @void: Empty parameter list for main.
-*
-* Description: Prints Holberton w/out stdio
-*
-* Return: 0 for success
+ * main - Prints Holberton
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints Holberton w/out stdio
+ *
+ * Return: 0 for success
 */
 int main(void)
-{
-	printf("_putchar");
+{	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
