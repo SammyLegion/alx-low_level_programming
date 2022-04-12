1bcff4d9f56da2e51df0205cae691d3d40095b77
root <sammyokorie0@gmail.com>
2022-04-12 16:00:27 -0700

    tch

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-/*
-*
-* print_alphabet - prints the alphabet, in lowercase, followed by a new line
+/**
+*print_alphabet - Prints the alphabet in lowercase followed by a new line
 *
 */
 
