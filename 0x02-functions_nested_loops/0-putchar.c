98bbd58e7c73bc8223ea423460cd6d5ced95fbe0
Samuel Okorie <sammyokorie0@gmail.com>
2022-04-12 22:17:31 +0100

    gcc stuff

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
 * main - Prints Holberton
@@ -10,21 +10,6 @@
 */
 int main(void)
 {
-	_putchar('_');
-
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-	
+	printf("_putchar");
 	return (0);
 }
