02e9dd21146568dd22d7f4a88cf566697c075520
Samuel Okorie <sammyokorie0@gmail.com>
2022-04-24 22:34:38 +0100

    please

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -12,5 +10,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-
-#endif
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
